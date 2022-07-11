5ce4e316ed98124074a723b344d58f949a5c720d
dkwiecien <dominikkwiecienzsb@gmail.com>
2022-07-11 13:15:24 +0200

    create navbar as a component

---
+++ react/src/components/Navbar.js
@@ -0,0 +1,36 @@
+import Styles from './Navbar.module.css';
+
+function Navbar{
+    return(
+        <div id="menu">
+            <div className="navbar">
+                <div id="logo">
+                  <img src={logo} alt="logo" />
+                </div>    
+                <ul className="main-nav">
+                <li className="nav-link active">
+                  <div className="nav-item">
+                    <a className="nav-item" href="#">Home</a>
+                  </div>
+                </li>
+                <li className="nav-link">
+                  <div className="nav-item">
+                    <a className="nav-item" href="#">Menu2</a>
+                  </div>
+                </li>
+                <li className="nav-link">
+                  <div className="nav-item">
+                   <a className="nav-item" href="#">Menu3</a>
+                  </div>
+                </li>
+                <li className="nav-link ">
+                 <div className="nav-item">
+                   <a className="nav-item" href="#">Pogoda</a>
+                  </div>
+                </li>
+                </ul>
+            </div>
+        </div>
+
+    );
+}
