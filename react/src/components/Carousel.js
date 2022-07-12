5c72328120462c2712684f51abfd6a9cb16ecff0
Mateusz-N <nosel.mateusz@gmail.com>
2022-07-12 14:07:42 +0200

    Polish the textbox

--- react/src/components/Carousel.js
+++ react/src/components/Carousel.js
@@ -40,13 +40,56 @@ function Carousel() {
                     </div>
                 </div>
                 <div id={Styles.textBox}>
+
                     <div id={Styles.textBoxHeader}>
                         <img src={Icon} alt="Icon" />
                         <p>Title</p>
                     </div>
                     <div id={Styles.textBoxContent}>
-                        
+                        <div className={Styles.textBoxItem}>
+                            <div className={Styles.textBoxItemNumber}>
+                                <p>1</p>
+                                <div></div>
+                            </div>
+                            <p className={Styles.textBoxItemText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac neque bibendum, vehicula ipsum et, tincidunt ligula.</p>
+                        </div>
+                        <div className={Styles.textBoxItem}>
+                            <div className={Styles.textBoxItemNumber}>
+                                <p>2</p>
+                                <div></div>
+                            </div>
+                            <p className={Styles.textBoxItemText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac neque bibendum, vehicula ipsum et, tincidunt ligula.</p>
+                        </div>
+                        <div className={Styles.textBoxItem}>
+                            <div className={Styles.textBoxItemNumber}>
+                                <p>3</p>
+                                <div></div>
+                            </div>
+                            <p className={Styles.textBoxItemText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac neque bibendum, vehicula ipsum et, tincidunt ligula.</p>
+                        </div>
+                        <div className={Styles.textBoxItem}>
+                            <div className={Styles.textBoxItemNumber}>
+                                <p>4</p>
+                                <div></div>
+                            </div>
+                            <p className={Styles.textBoxItemText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac neque bibendum, vehicula ipsum et, tincidunt ligula.</p>
+                        </div>
+                        <div className={Styles.textBoxItem}>
+                            <div className={Styles.textBoxItemNumber}>
+                                <p>5</p>
+                                <div></div>
+                            </div>
+                            <p className={Styles.textBoxItemText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac neque bibendum, vehicula ipsum et, tincidunt ligula.</p>
+                        </div>
+                        <div className={Styles.textBoxItem}>
+                            <div className={Styles.textBoxItemNumber}>
+                                <p>6</p>
+                                <div></div>
+                            </div>
+                            <p className={Styles.textBoxItemText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac neque bibendum, vehicula ipsum et, tincidunt ligula.</p>
+                        </div>
                     </div>
+
                 </div>
             </div>
         </div>
