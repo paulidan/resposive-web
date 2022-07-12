3818cd23551da43609796ecaca981821663e38d5
Mateusz-N <nosel.mateusz@gmail.com>
2022-07-12 15:13:10 +0200

    Tidy up the structure and improve styling

--- react/src/components/Header.js
+++ react/src/components/Header.js
@@ -1,33 +1,30 @@
 import HeaderStyle from "./Header.module.css";
-import layer_7 from '../icons/desktop/Layer_7.png';
-import layer_11 from '../icons/desktop/Layer_11.png';
-import layer_8 from '../icons/desktop/Layer_8.png';
-import layer_12 from '../icons/desktop/Layer_12.png';
+import layer_7_c from '../icons/desktop/Layer_7_c.png';
+import layer_11_c from '../icons/desktop/Layer_11_c.png';
+import layer_8_cr from '../icons/desktop/Layer_8_cr.png';
+import layer_12_cq from '../icons/desktop/Layer_12_cq.png';
 import layer_9 from '../icons/desktop/Layer_9.png';
 import layer_10 from '../icons/desktop/Layer_10.png';
 import layer_37 from '../icons/desktop/Layer_37.png';
 
 function Header(){
     return(
-      <div id={HeaderStyle.header}> 
-        <div className={HeaderStyle.background}>
-          <img src={layer_37} alt="background pic" />
-          <div id={HeaderStyle.heading}>
-            <h1> Feel the Power of Typography</h1>
+      <div id={HeaderStyle.background}>
+        <div id={HeaderStyle.heading}>
+          <p>Feel the Power of Typography</p>
+        </div>
+        <div id={HeaderStyle.boxes}>
+          <div className={HeaderStyle.red_boxes}>
+            <img src={layer_7_c} alt="typo box" />
+            <img src={layer_11_c} alt="Typography" />
           </div>
-          <div className={HeaderStyle.boxes}>
-            <div className={HeaderStyle.red_boxes}>
-              <img src={layer_7} alt="typo box" />
-              <img src={layer_11} alt="Typography" />
-            </div>
-            <div className={HeaderStyle.blue_boxes}>
-              <img src={layer_8} alt="resp box" />
-              <img src={layer_12} alt="Responsive" />
-            </div>
-            <div className={HeaderStyle.green_boxes}>
-              <img src={layer_9} alt="ux box" />
-              <img src={layer_10} alt="UX" />
-            </div>
+          <div className={HeaderStyle.blue_boxes}>
+            <img src={layer_8_cr} alt="resp box" />
+            <img src={layer_12_cq} alt="Responsive" />
+          </div>
+          <div className={HeaderStyle.green_boxes}>
+            <img src={layer_9} alt="ux box" />
+            <img src={layer_10} alt="UX" />
           </div>
         </div>
       </div>
