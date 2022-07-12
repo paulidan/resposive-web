d02ccf62cd5e8bdbef644ce9f366656864006880
Mateusz-N <nosel.mateusz@gmail.com>
2022-07-12 12:50:04 +0200

    Add bottom photo styling and textbox header

--- react/src/components/Carousel.js
+++ react/src/components/Carousel.js
@@ -4,6 +4,7 @@ import Clip_bo from '../icons/desktop/Clip_bo.png';
 import Clip_bj from '../icons/desktop/Clip_bj.png';
 import Clip_ from '../icons/desktop/Clip_.png';
 import Clip from '../icons/desktop/Clip.png';
+import Icon from '../icons/desktop/Icon.png';
 import Styles from './Carousel.module.css';
 
 function Carousel() {
@@ -27,13 +28,25 @@ function Carousel() {
             </div>
             <div id={Styles.bottomItemContainer}>
                 <div className={Styles.bottomPhotoBox} id={Styles.bottomPhotoBox1}>
-                    <img src={Clip_} alt="Bottom Photo 1" />
+                    <img src={Clip_} alt="Bottom Photo 1"></img>
+                    <div className={Styles.bottomPhotoOverlay} id={Styles.bottomPhotoOverlay1}>
+                        <p>»</p>
+                    </div>
                 </div>
                 <div className={Styles.bottomPhotoBox} id={Styles.bottomPhotoBox2}>
                     <img src={Clip} alt="Bottom Photo 2" />
+                    <div className={Styles.bottomPhotoOverlay} id={Styles.bottomPhotoOverlay2}>
+                        <p>«</p>
+                    </div>
                 </div>
                 <div id={Styles.textBox}>
-
+                    <div id={Styles.textBoxHeader}>
+                        <img src={Icon} alt="Icon" />
+                        <p>Title</p>
+                    </div>
+                    <div id={Styles.textBoxContent}>
+                        
+                    </div>
                 </div>
             </div>
         </div>
