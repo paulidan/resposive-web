63596202fe5d5ac767c8a6116046f05823e2ba06
paulidan <paulina9913@gmail.com>
2022-07-13 15:41:06 +0200

    searchbar as component
    work on search bar in progress + connect api to see city prompts

--- react/src/components/Weather/CurrentWeather.js
+++ react/src/components/Weather/CurrentWeather.js
@@ -1,32 +1,40 @@
 import React, { useState } from 'react';
 import CurrentStyle from './CurrentW.module.css';
+import SearchStyle from './Search.module.css';
 import sun from '../../icons/desktop/weathersun.png';
 import rain from '../../icons/desktop/weatherrain.png';
 import cloud from '../../icons/desktop/weathercloud.png';
+import { weatherApi } from './Api';
+// import Search from './Search';
 
-const api = {
-    key: 'fd54d9727b22aa867dc1f33db65eaafc',
-    base: 'https://api.openweathermap.org/data/2.5/'
-}
 
-function CurrentWeather(){
+function CurrentWeather() {
 
     const [query, setQuery] = useState('');
     const [weather, setWeather] = useState({});
 
-    const Search = evt => {
-        console.log(evt);
-        if(evt.code === 'Enter'){
-            fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-            .then(res => res.json())
-            .then(result => {
-                setWeather(result);
-                setQuery('');
-                console.log(result);
-            });
-        }
+    // const handleOnSearchChange = (searchData) => {
+    //     const [lat, lon] = searchData.value.split(" ");
 
-    }
+    //     const searching = fetch(
+    //         fetch(`${weatherApi.base}weather?q=${query}&units=metric&APPID=${weatherApi.key}`)
+    //             .then(res => res.json())
+    //     )
+            const Search = evt => {
+                console.log(evt);
+                if(evt.code === 'Enter'){
+                    fetch(`${weatherApi.base}weather?q=${query}&units=metric&APPID=${weatherApi.key}`)
+                    .then(res => res.json())
+                    .then(result => {
+                        setWeather(result);
+                        setQuery('');
+                        console.log(result);
+                    });
+                }
+
+            }
+    //         .catch(console.log);
+    // }
 
     const icons = {
         'Clear': sun,
@@ -48,42 +56,44 @@ function CurrentWeather(){
         return `${d.toLocaleString()}`
     }
 
-    return(
+    return (
         <div className={CurrentStyle.app}>
             <main>
-                <div className={CurrentStyle.search_box}>
+                <div className={SearchStyle.search_box}>
                     <input 
                     type='text'
-                    className={CurrentStyle.search_bar}
+                    className={SearchStyle.search_bar}
                     placeholder='Search...'
                     onChange={e => setQuery (e.target.value)}
                     value= {query}
                     onKeyPress={Search}
                     />
+                    {/* <Search onSearchChange={handleOnSearchChange} /> */}
                 </div>
-                {(typeof weather.main != 'undefined')?(
+                {(typeof weather.main != 'undefined') ? (
                     <div>
                         <div className={CurrentStyle.location_box}>
                             <div className={CurrentStyle.location}>{weather.name}, {weather.sys.country}</div>
-                                <div className={CurrentStyle.date}>{dateBuilder(new Date())}</div>
-                                </div>
-                                <div className={CurrentStyle.weather_box}>
-                                    <div className={CurrentStyle.temp}>
-                                        {Math.round(weather.main.temp)}°C
-                                        <div className={CurrentStyle.feels}>
-                                            feels like {Math.round(weather.main.feels_like)}°C
-                                        </div>
-                                        <div className={CurrentStyle.weather}>
-                                            {weather.weather[0].main} <img src={icons[weather.weather[0].main]} />
-                                    </div>
-                                    </div>
-
-                                </div> 
+                            <div className={CurrentStyle.date}>{dateBuilder(new Date())}</div>
                         </div>
-                    
-                ) : ('') }
+                        <div className={CurrentStyle.weather_box}>
+                            <div className={CurrentStyle.temp}>
+                                {Math.round(weather.main.temp)}°C
+                                <div className={CurrentStyle.feels}>
+                                    feels like {Math.round(weather.main.feels_like)}°C
+                                </div>
+                                <div className={CurrentStyle.weather}>
+                                    {weather.weather[0].main} <img src={icons[weather.weather[0].main]} />
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                ) : ('')}
             </main>
         </div>
     );
 }
+
 export default CurrentWeather;
