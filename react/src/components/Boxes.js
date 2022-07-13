7f4c118cf9b3391893257f2d4446e9e5e2e4c974
Mateusz-N <nosel.mateusz@gmail.com>
2022-07-13 12:25:16 +0200

    Add initial smartphone layout, fix general styling a bit

--- react/src/components/Boxes.js
+++ react/src/components/Boxes.js
@@ -13,21 +13,23 @@ function Boxes() {
                 <div className={styles.subBox} id={styles.bottomRightBox}>
                     <p>BOX NAME 4</p>
                 </div>
-                <div className={styles.subBox} id={styles.centerBoxOutline}>
-                    
+                <div id={styles.centerBoxContainer}>
+                    <div className={styles.subBox} id={styles.centerBoxOutline}>
+                        
+                    </div> 
+                    <div className={styles.subBox} id={styles.centerBox}>
+                        <div id={styles.centerBoxInner}>
+                            <p>
+                                <span id={styles.centerBoxHeader}>
+                                    Text from box name 1
+                                </span>
+                                <span id={styles.centerBoxText}>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ornare turpis nec mauris scelerisque, sed suscipit elit dapibus. Aliquam ut eros sed risus convallis venenatis eget vitae mi. Cras dictum est non nunc semper blandit. Quisque convallis tristique dolor eget ultrices. Mauris ac orci quis neque facilisis laoreet. Suspendisse vitae ultrices ipsum, at placerat eros. Proin varius malesuada ultricies. Cras a metus elementum, pellentesque libero eu, faucibus lorem. Integer ac ornare lacus. Ut pretium felis tellus, nec faucibus eros rhoncus eget. Pellentesque ultrices, ligula vestibulum porttitor pharetra, arcu nisl vulputate ligula, id vulputate ligula urna vel tortor. In ullamcorper blandit erat, nec egestas massa commodo consectetur. In nec odio lorem. Donec nulla neque, vehicula in bibendum et, rhoncus sit amet nibh.
+                                </span>
+                            </p>
+                        </div>
+                    </div> 
                 </div> 
-                <div className={styles.subBox} id={styles.centerBox}>
-                    <div id={styles.centerBoxInner}>
-                        <p>
-                            <span id={styles.centerBoxHeader}>
-                                Text from box name 1
-                            </span>
-                            <span id={styles.centerBoxText}>
-                                Text from box name 1Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ornare turpis nec mauris scelerisque, sed suscipit elit dapibus. Aliquam ut eros sed risus convallis venenatis eget vitae mi. Cras dictum est non nunc semper blandit. Quisque convallis tristique dolor eget ultrices. Mauris ac orci quis neque facilisis laoreet. Suspendisse vitae ultrices ipsum, at placerat eros. Proin varius malesuada ultricies. Cras a metus elementum, pellentesque libero eu, faucibus lorem. Integer ac ornare lacus. Ut pretium felis tellus, nec faucibus eros rhoncus eget. Pellentesque ultrices, ligula vestibulum porttitor pharetra, arcu nisl vulputate ligula, id vulputate ligula urna vel tortor. In ullamcorper blandit erat, nec egestas massa commodo consectetur. In nec odio lorem. Donec nulla neque, vehicula in bibendum et, rhoncus sit amet nibh.
-                            </span>
-                        </p>
-                    </div>
-                </div>  
                 <div className={styles.subBox} id={styles.topLeftBoxOutline}>
                 </div>
                 <div className={styles.subBox} id={styles.topLeftBox}>
