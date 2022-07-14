80ce401db85ae86fc4c97adcbbfd7d615a9b2997
dkwiecien <dominikkwiecienzsb@gmail.com>
2022-07-14 17:13:39 +0200

    solve problems from github

--- react/src/components/ButtonsPogoda.js
+++ react/src/components/ButtonsPogoda.js
@@ -2,12 +2,8 @@ import ButtonsPogodaStyle from "./ButtonsPogoda.module.css";
 import React, {components} from 'react';
 import { Link } from "react-router-dom";
 
-
-
 function ButtonsPogoda()  {
     return(
-        
-        
         <div className={ButtonsPogodaStyle.ButtonsMenu}>
             <Link to={"/pogoda/szukaj"}>
                 <button className={ButtonsPogodaStyle.Buttons}>Szukaj pogody</button>
@@ -19,9 +15,6 @@ function ButtonsPogoda()  {
                 <button className={ButtonsPogodaStyle.Buttons}>Pogoda długoterminowa</button>
             </Link>
         </div>
-       
-        
-
     );
 }
 export default ButtonsPogoda;
