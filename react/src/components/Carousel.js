5d5e59ccf561df39e83d2b7c4d5c4e2d57ed8fb1
Mateusz-N <nosel.mateusz@gmail.com>
2022-07-20 10:28:38 +0200

    Add tablet layout

--- react/src/components/Carousel.js
+++ react/src/components/Carousel.js
@@ -6,6 +6,10 @@ import img_phone_topPhotoBox1 from '../icons/smartphone/Clip_bo.png';
 import img_phone_topPhotoBox2 from '../icons/smartphone/Clip_bk.png';
 import img_phone_topPhotoBox3 from '../icons/smartphone/Clip_be.png';
 import img_phone_topPhotoBox4 from '../icons/smartphone/Clip_.png';
+import img_tablet_topPhotoBox1 from '../icons/tablet/Clip_bt.png';
+import img_tablet_topPhotoBox2 from '../icons/tablet/Clip_bk.png';
+import img_tablet_topPhotoBox3 from '../icons/tablet/Clip_bp.png';
+import img_tablet_topPhotoBox4 from '../icons/tablet/Clip_bg.png';
 import CarouselTextbox from './CarouselTextbox';
 import Styles from './Carousel.module.css';
 
@@ -17,19 +21,23 @@ function Carousel() {
                     <div className={Styles.topPhotoBox} id={Styles.topPhotoBox1}>
                         <img src={img_desk_topPhotoBox1} alt="Top Photo 1" className={Styles.topPhotoDesktop}/>
                         <img src={img_phone_topPhotoBox1} alt="Top Photo 1" className={Styles.topPhotoPhone}/>
+                        <img src={img_tablet_topPhotoBox1} alt="Top Photo 1" className={Styles.topPhotoTablet}/>
                     </div>
                     <div className={Styles.topPhotoBox} id={Styles.topPhotoBox2}>
                         <img src={img_desk_topPhotoBox2} alt="Top Photo 2" className={Styles.topPhotoDesktop}/>
                         <img src={img_phone_topPhotoBox2} alt="Top Photo 2" className={Styles.topPhotoPhone}/>
+                        <img src={img_tablet_topPhotoBox2} alt="Top Photo 2" className={Styles.topPhotoTablet}/>
                     </div>
                     <div className={Styles.topPhotoBox} id={Styles.topPhotoBox3}>
                         <img src={img_desk_topPhotoBox3} alt="Top Photo 3" className={Styles.topPhotoDesktop}/>
                         <img src={img_phone_topPhotoBox3} alt="Top Photo 3" className={Styles.topPhotoPhone}/>
+                        <img src={img_tablet_topPhotoBox3} alt="Top Photo 3" className={Styles.topPhotoTablet}/>
                         <p><span className={Styles.boxCaptionLine}>BOX</span> <span className={Styles.boxCaptionLine}>CAPTION</span></p>
                     </div>
                     <div className={Styles.topPhotoBox} id={Styles.topPhotoBox4}>
                         <img src={img_desk_topPhotoBox4} alt="Top Photo 4" className={Styles.topPhotoDesktop}/>
                         <img src={img_phone_topPhotoBox4} alt="Top Photo 4" className={Styles.topPhotoPhone}/>
+                        <img src={img_tablet_topPhotoBox4} alt="Top Photo 4" className={Styles.topPhotoTablet}/>
                         <p><span className={Styles.boxCaptionLine}>BOX</span> <span className={Styles.boxCaptionLine}>CAPTION</span></p>
                     </div>
                 </div>
