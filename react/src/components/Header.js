86d83ce02328784b1802483522a0b15ef8cb9f42
Mateusz-N <nosel.mateusz@gmail.com>
2022-07-22 22:26:00 +0200

    Reorder imports, use const functions, fix absolute path configs

--- react/src/components/Header.js
+++ react/src/components/Header.js
@@ -34,4 +34,4 @@ function Header(){
     );
 }
 
-export default Header
+export default Header;
--- react/src/components/Navbar.js
+++ react/src/components/Navbar.js
@@ -1,7 +1,6 @@
+import { Link } from 'react-router-dom';
 import Styles from './Navbar.module.css';
 import logo from '@/icons/desktop/Logo.png';
-import { Link } from 'react-router-dom';
-
 
 function Navbar(){
     return(
@@ -34,7 +33,6 @@ function Navbar(){
                 </ul>
             </div>
         </div>
-
     );
 }
 
