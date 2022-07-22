84d8263d4ead8896f9583aca4157e5c721588ac6
paulidan <paulina9913@gmail.com>
2022-07-22 11:50:56 +0200

    work on axios

--- react/src/pages/WeatherSearchPage.js
+++ react/src/pages/WeatherSearchPage.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Navbar from '../components/Navbar';
 import Header from '../components/Header';
 import ButtonsWeather from '../components/ButtonsWeather';
@@ -10,38 +11,57 @@ import Forecast from '../components/Weather/Forecast/Forecast';
 const WeatherSearchPage = () => {
   const [currentWeather, setCurrentWeather] = useState(null);
   const [forecast, setForecast] = useState(null);
+  const [lat, setLat] = useState([]);
+  const [lon, setLon] = useState([]);
 
   const handleSearch = (searchData) => {
-    const [lat, lon] = searchData.value.split(" ");
+    // const [lat, lon] = searchData.value.split(" ");
 
-    const fetchCurrentWeather = fetch(
-      `${weatherApi.base}/weather?lat=${lat}&lon=${lon}&appid=${weatherApi.key}&units=metric`
-    );
-    const forecastFetch = fetch(
-      `${weatherApi.base}/forecast?lat=${lat}&lon=${lon}&appid=${weatherApi.key}&units=metric`
-    );
-
-    Promise.all([fetchCurrentWeather, forecastFetch])
-      .then(async (response) => {
-        const weatherResponse = await response[0].json();
-        const forcastResponse = await response[1].json();
-
-        setCurrentWeather({ city: searchData.label, ...weatherResponse });
-        setForecast({ city: searchData.label, ...forcastResponse });
-      })
-      .catch(console.log);
+   
   }
+  const getCurrentWeather = (lat, lon) => {
+    axios.get(`${weatherApi.base}/weather?lat=${lat}&lon=${lon}&appid=${weatherApi.key}&units=metric`)
+    .then((response) => {
+      console.log(setCurrentWeather)
+    })
+  }
+  const getForecast = (lat, lon) => {
+    axios.get(`${weatherApi.base}/forecast?lat=${lat}&lon=${lon}&appid=${weatherApi.key}&units=metric`)
+    .then((response) => {
+      console.log(setForecast)
+    })
+  }
+  useEffect(() => {
+    getCurrentWeather(lat, lon);
+    getForecast(lat, lon);
+    navigator.geolocation.getCurrentPosition(function (position) {
+      setLat(position.coords.latitude);
+      setLon(position.coords.longitude);
+    
+  }, [lat, lon])
+})
+
+  //   Promise.all([getCurrentWeather, getForecast])
+  //     .then(async (response) => {
+  //       const weatherResponse = await response[0].json();
+  //       const forcastResponse = await response[1].json();
+
+        
+  //       ;
+  //     })
+  //     .catch(console.log);
+  // }
 
   return (
     <div className="App">
-        <Header />
-        <Navbar />
-        <ButtonsWeather />
-        <Search onSearchChange={handleSearch} />
-        {currentWeather && <CurrentWeather data={currentWeather} />}
-        {forecast && <Forecast data={forecast} />}
+      <Header />
+      <Navbar />
+      <ButtonsWeather />
+      <Search onSearchChange={handleSearch} />
+      {currentWeather && <CurrentWeather data={currentWeather} />}
+      {forecast && <Forecast data={forecast} />}
     </div>
   );
-}
-
+  }
+  
 export default WeatherSearchPage;
