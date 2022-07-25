97a996d362e91dad396758acf12318c84b721907
Mateusz-N <nosel.mateusz@gmail.com>
2022-07-25 12:42:50 +0200

    Make TextboxContent a separate component

--- react/src/components/CarouselTextbox.js
+++ react/src/components/CarouselTextbox.js
@@ -1,4 +1,5 @@
 import Icon from '@/icons/desktop/Icon.png';
+import TextboxContent from './TextboxContent';
 import Styles from './CarouselTextbox.module.css';
 
 const CarouselTextbox = () => {
@@ -8,50 +9,7 @@ const CarouselTextbox = () => {
                 <img src={Icon} alt="Icon" />
                 <p>Title</p>
             </div>
-            <ol id={Styles.textBoxContent}>
-                <li className={Styles.textBoxItem}>
-                    <div className={Styles.textBoxItemNumber}>
-                        <p>1</p>
-                        <div />
-                    </div>
-                    <p className={Styles.textBoxItemText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac neque bibendum, vehicula ipsum et, tincidunt ligula.</p>
-                </li>
-                <li className={Styles.textBoxItem}>
-                    <div className={Styles.textBoxItemNumber}>
-                        <p>2</p>
-                        <div />
-                    </div>
-                <p className={Styles.textBoxItemText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac neque bibendum, vehicula ipsum et, tincidunt ligula.</p>
-                </li>
-                <li className={Styles.textBoxItem}>
-                    <div className={Styles.textBoxItemNumber}>
-                        <p>3</p>
-                        <div />
-                    </div>
-                    <p className={Styles.textBoxItemText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac neque bibendum, vehicula ipsum et, tincidunt ligula.</p>
-                </li>
-                <li className={Styles.textBoxItem}>
-                    <div className={Styles.textBoxItemNumber}>
-                        <p>4</p>
-                        <div />
-                    </div>
-                    <p className={Styles.textBoxItemText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac neque bibendum, vehicula ipsum et, tincidunt ligula.</p>
-                </li>
-                <li className={Styles.textBoxItem}>
-                    <div className={Styles.textBoxItemNumber}>
-                        <p>5</p>
-                        <div />
-                    </div>
-                    <p className={Styles.textBoxItemText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac neque bibendum, vehicula ipsum et, tincidunt ligula.</p>
-                </li>
-                <li className={Styles.textBoxItem}>
-                    <div className={Styles.textBoxItemNumber}>
-                        <p>6</p>
-                        <div />
-                    </div>
-                    <p className={Styles.textBoxItemText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac neque bibendum, vehicula ipsum et, tincidunt ligula.</p>
-                </li>
-            </ol>
+            <TextboxContent />
         </div>
     );
 }
--- react/src/pages/Home.js
+++ react/src/pages/Home.js
@@ -8,7 +8,7 @@ const HomePage = () => {
         <div className="App">
             <Header />
             <Navbar />
-            <Boxes />
+            {/* <Boxes /> */}
             <Carousel />
         </div>
     );
