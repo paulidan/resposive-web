7d4233f4e1f3b5a3ca431cf41796ddaf0006b2d7
Mateusz-N <nosel.mateusz@gmail.com>
2022-07-25 13:02:24 +0200

    Make TextboxItem a separate component, generate the items programatically

--- react/src/components/TextboxContent.js
+++ react/src/components/TextboxContent.js
@@ -1,50 +1,16 @@
+import TextboxItem from './TextboxItem';
 import Styles from './TextboxContent.module.css';
+
+const loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac neque bibendum, vehicula ipsum et, tincidunt ligula.";
+const content = [];
+for(let i = 1; i <=6; i++) {
+    content.push(<TextboxItem itemNumber={i} itemText={loremIpsum} />);
+}
 
 const TextboxContent = () => {
     return(
         <ol id={Styles.textBoxContent}>
-            <li className={Styles.textBoxItem}>
-                <div className={Styles.textBoxItemNumber}>
-                    <p>1</p>
-                    <div />
-                </div>
-                <p className={Styles.textBoxItemText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac neque bibendum, vehicula ipsum et, tincidunt ligula.</p>
-            </li>
-            <li className={Styles.textBoxItem}>
-                <div className={Styles.textBoxItemNumber}>
-                    <p>2</p>
-                    <div />
-                </div>
-            <p className={Styles.textBoxItemText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac neque bibendum, vehicula ipsum et, tincidunt ligula.</p>
-            </li>
-            <li className={Styles.textBoxItem}>
-                <div className={Styles.textBoxItemNumber}>
-                    <p>3</p>
-                    <div />
-                </div>
-                <p className={Styles.textBoxItemText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac neque bibendum, vehicula ipsum et, tincidunt ligula.</p>
-            </li>
-            <li className={Styles.textBoxItem}>
-                <div className={Styles.textBoxItemNumber}>
-                    <p>4</p>
-                    <div />
-                </div>
-                <p className={Styles.textBoxItemText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac neque bibendum, vehicula ipsum et, tincidunt ligula.</p>
-            </li>
-            <li className={Styles.textBoxItem}>
-                <div className={Styles.textBoxItemNumber}>
-                    <p>5</p>
-                    <div />
-                </div>
-                <p className={Styles.textBoxItemText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac neque bibendum, vehicula ipsum et, tincidunt ligula.</p>
-            </li>
-            <li className={Styles.textBoxItem}>
-                <div className={Styles.textBoxItemNumber}>
-                    <p>6</p>
-                    <div />
-                </div>
-                <p className={Styles.textBoxItemText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac neque bibendum, vehicula ipsum et, tincidunt ligula.</p>
-            </li>
+            {content}
         </ol>
     );
 }
