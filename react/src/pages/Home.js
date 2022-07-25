3b2274068ba925b6a48938f4cfbdcb2cc2d96e5a
Mateusz-N <nosel.mateusz@gmail.com>
2022-07-25 13:21:00 +0200

    Move Carousel to its own subfolder, fix file structure

--- react/src/pages/Home.js
+++ react/src/pages/Home.js
@@ -1,14 +1,14 @@
 import Navbar from '@/components/Navbar';
 import Header from '@/components/Header';
 import Boxes from '@/components/Boxes';
-import Carousel from '@/components/Carousel';
+import Carousel from '@/components/carousel/Carousel';
 
 const HomePage = () => {
     return (
         <div className="App">
             <Header />
             <Navbar />
-            {/* <Boxes /> */}
+            <Boxes />
             <Carousel />
         </div>
     );
