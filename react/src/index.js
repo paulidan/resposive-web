cf36298a2eaeb4e08949e2a549a5944c4c4e7acb
Mateusz-N <nosel.mateusz@gmail.com>
2022-07-27 12:29:15 +0200

    Use hooks to generate TextboxContent

--- react/src/index.js
+++ react/src/index.js
@@ -7,11 +7,9 @@ import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter> 
+  <BrowserRouter> 
     <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
