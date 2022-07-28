3d8470f9fbff5659604581fd142b6d6ac37fb558
Mateusz-N <nosel.mateusz@gmail.com>
2022-07-28 09:37:15 +0200

    Improve tablet and smartphone layouts

--- react/src/components/Navbar.js
+++ react/src/components/Navbar.js
@@ -1,11 +1,13 @@
 import { NavLink, Link } from 'react-router-dom';
 import logo from '@/icons/desktop/Logo.png';
+import img_tablet_icon from '@/icons/tablet/Icon_cb.png';
+import img_phone_icon from '@/icons/smartphone/Icon_b.png';
 import Styles from './Navbar.module.css';
 
 const Navbar = () => {
     return(
       <div id={Styles.navbar}>
-          <img src={logo} alt="logo" id={Styles.logo} />
+          <img src={logo} alt="fireup.pro - power to change IT" id={Styles.logo} />
           <ul id={Styles.mainNav}>
             <li className={Styles.navItem} active="true">
               <NavLink className={Styles.navLink} activeclassname={Styles.navItem} to="/">HOME</NavLink>
@@ -20,6 +22,8 @@ const Navbar = () => {
               <Link className={Styles.navLink} to="/pogoda">POGODA</Link>
             </li>
           </ul>
+          <img src={img_tablet_icon} alt="Open menu" id={Styles.navbarIconTablet}/>
+          <img src={img_phone_icon} alt="Open menu" id={Styles.navbarIconPhone}/>
       </div>
     );
 }
